2e0bf48d223754370e993c3874f1e74a04b5c655
Kilian <dankestmirror@hotmail.es>
2025-06-06 17:29:46 +0100

    Aplicando estilos globales

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,48 @@
 </script>
 
 <template>
-
+  <div>
+    <header>
+      <h1>Admin Gastos</h1>
+    </header>
+  </div>
 </template>
 
-<style scoped>
-
+<style>
+  :root {
+    --azul: #3b82f6;
+    --blanco: #fff;
+    --gris-claro: #f5f5f5;
+    --gris: #94a3b8;
+    --gris-oscuro: #64748b;
+    --negro: #1f2937;
+  }
+  html{
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+  body{
+    font-size: 1.6rem;
+    font-family: 'Lato', sans-serif;
+    background-color: var(--gris-claro);
+  }
+  h1{
+    font-size: 4rem;
+  }
+  h2{
+    font-size: 3rem;
+  }
+  header{
+    background-color: var(--azul);
+  }
+  header h1{
+    padding: 3rem 0;
+    margin: 0;
+    color: var(--blanco);
+    text-align: center;
+  }
+  
 </style>
