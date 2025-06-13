742bcd02584e5622d0b00b2ede5514067377394f
Kilian <dankestmirror@hotmail.es>
2025-06-13 16:53:45 +0100

    Formateador de Fechas

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -9,3 +9,13 @@ export const generarId = () => {
     const random = Math.random().toString(36).substring(2)
     return fecha + random
 }
+
+export const formatearFecha = (fecha) => {
+    const fechaNueva = new Date(fecha)
+    const opciones = {
+     year:'numeric',
+    month:'long',
+    day:'2-digit'
+    }
+    return fechaNueva.toLocaleDateString('es-ES',opciones)
+}
